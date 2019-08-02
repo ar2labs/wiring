c40d01f3c661b952c57acf628b4d2bc3925e9976
Anderson Costa <arcostasi@gmail.com>
2019-08-02 10:09:39 -0300

    fix: improve error handler responses

--- src/Interfaces/ErrorHandlerInterface.php
+++ src/Interfaces/ErrorHandlerInterface.php
@@ -1,8 +1,31 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Wiring\Interfaces;
 
 interface ErrorHandlerInterface
 {
+    /**
+     * Return an error into an HTTP or JSON data array.
+     *
+     * @param string $mesg
+     *
+     * @return array
+     */
+    public function error(?string $mesg = null): array;
+
+    /**
+     * Get exception.
+     *
+     * @return \Exception|\Throwable
+     */
+    public function getException();
+
+    /**
+     * Check is JSON.
+     *
+     * @return bool
+     */
+    public function isJson(): bool;
 }
