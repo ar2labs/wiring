c15cd0a20d69c74d291686921ce2de40e78dc85a
Vinicius <vinyvicente@gmail.com>
2019-08-12 18:41:24 -0300

    test: increase coverage

--- src/Http/Exception/BadRequestException.php
+++ src/Http/Exception/BadRequestException.php
@@ -4,20 +4,18 @@ declare(strict_types=1);
 
 namespace Wiring\Http\Exception;
 
-use Exception;
-
 class BadRequestException extends HttpException
 {
     /**
      * Constructor
      *
      * @param string    $message
-     * @param Exception $previous
+     * @param \Exception $previous
      * @param int $code
      */
     public function __construct(
         string $message = 'Bad Request',
-        ?Exception $previous = null,
+        ?\Exception $previous = null,
         int $code = 0
     ) {
         parent::__construct(400, $message, $previous, [], $code);
