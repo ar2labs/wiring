103a4c56d4b32c23d69a2a085bc63b3d9076ba64
Anderson Costa <arcostasi@gmail.com>
2019-10-07 13:58:57 -0300

    Request handler coverage

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -9,20 +9,44 @@ use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Wiring\Interfaces\ApplicationInterface;
 
 final class ApplicationTest extends TestCase
 {
+    /**
+     * @throws \Exception
+     */
     public function testInstanceCreated()
     {
         $container = $this->createContainerMock();
         $request = $this->createServerRequestMock();
         $response = $this->createResponseMock();
+        $middleware = $this->createMiddlewareMock();
 
         $app = new Application($container, $request, $response);
 
         $this->assertInstanceOf(Application::class, $app);
         $this->assertInstanceOf(ResponseInterface::class, $app->run());
         $this->assertInstanceOf(ResponseInterface::class, $app->stop());
+        $this->assertInstanceOf(ApplicationInterface::class,
+            $app->addMiddleware($middleware, 'test'));
+        $this->assertInstanceOf(ApplicationInterface::class,
+            $app->addAfterMiddleware($middleware, 'test'));
+        $this->assertInstanceOf(MiddlewareInterface::class,
+            $app->getMiddleware('test'));
+        $this->assertInstanceOf(RequestHandlerInterface::class,
+            $app->removeMiddleware('test'));
+        $this->assertInstanceOf(RequestHandlerInterface::class,
+            $app->addRouterMiddleware($middleware));
+        $this->assertInstanceOf(RequestHandlerInterface::class,
+            $app->addRequestHandler($middleware));
+        $this->assertInstanceOf(RequestHandlerInterface::class,
+            $app->addEmitterMiddleware($middleware));
+        $this->assertIsBool($app->isAfterMiddleware());
+        $this->assertNull($app->getMiddleware('test2'));
+        $this->assertInstanceOf(ResponseInterface::class, $app->handle($request));
     }
 
     private function createContainerMock()
@@ -38,5 +62,10 @@ final class ApplicationTest extends TestCase
     private function createResponseMock()
     {
         return $this->createMock(ResponseInterface::class);
+    }
+
+    private function createMiddlewareMock()
+    {
+        return $this->createMock(MiddlewareInterface::class);
     }
 }
